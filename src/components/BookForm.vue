11fda34d1ad58365f98c5ffdf7d3acd506c4dd1d
Karan Khare <karan.khare@nio.io>
2019-05-15 13:39:40 -0700

    Updated book form

---
+++ src/components/BookForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form class="" actions="#" method="post" @submit.prevent="bookSubmit(bookTitle, bookAuthor)">
+    <input v-model="bookTitle" type="text" name="title" value="" placeholdere="Book Title"/>
+    <input v-model="bookAuthor" type="text" name="title" value="" placeholdere="Book Authot"/>
+    <button type="submit" name="button">Add Book</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'BookForm',
+  props: ['books'],
+  data() {
+    return {
+      bookTitle: '',
+      bookAuthor: ''
+    }
+  },
+  methods: {
+    bookSubmit(bookTitle, bookAuthor) {
+      this.$emit('addBook',bookTitle, bookAuthor);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
